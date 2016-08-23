a0bcb31b67101a46b03a9bb3ab80e616c3fbd3d4
吴磊 <953064942@qq.com>
2016-08-23 19:19:50 +0800

    添加了新的认证返回方式，如果type为1的话，那只返回openId；如果type为2的话，那返回当前微信用户信息。

--- wx_auth.js
+++ wx_auth.js
@@ -3,16 +3,23 @@ const async = require('async'),
       request = require('request'),
       config = require('./config');
 
+
 //获取微信用户信息
 exports.getWxUserInfor = function(req,res){
   async.waterfall([
   function(callback){ //获取access_token
-    let code = req.body.code,
+    let postData = JSON.parse(req.body.param),
+        code = postData.code,
+        type = Number(postData.type), //1.只获取openId  2.获取微信用户信息
         getTokenUrl = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=${config.wxConfig.appId}&secret=${config.wxConfig.appSecret}&code=${code}&grant_type=authorization_code`;
         request(getTokenUrl,function (error,response,body) {
             if (!error && response.statusCode == 200) {
                   let tokenData = JSON.parse(body);
-                  callback(null,tokenData);
+                  if(type == 1){
+                    res.send(tokenData)
+                  } else {
+                    callback(null,tokenData);
+                  }
               }
       })
   },function(tokenData,callback){ //获取微信用户信息
