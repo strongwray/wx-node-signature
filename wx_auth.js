2b95e6d53f35a90729b31891add3f9807106c50d
吴磊 <953064942@qq.com>
2016-08-23 15:55:05 +0800

    完成了微信中的获取用户信息。

---
+++ wx_auth.js
@@ -0,0 +1,30 @@
+'use strict'
+const async = require('async'),
+      request = require('request'),
+      config = require('../config');
+
+//获取微信用户信息
+exports.getWxUserInfor = function(req,res){
+  async.waterfall([
+  function(callback){ //获取access_token
+    let code = req.body.code,
+        getTokenUrl = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=${config.wxConfig.appId}&secret=${config.wxConfig.appSecret}&code=${code}&grant_type=authorization_code`;
+        request(getTokenUrl,function (error,response,body) {
+            if (!error && response.statusCode == 200) {
+                  let tokenData = JSON.parse(body);
+                  callback(null,tokenData);
+              }
+      })
+  },function(tokenData,callback){ //获取微信用户信息
+      let getInforUrl = `https://api.weixin.qq.com/sns/userinfo?access_token=${tokenData.access_token}&openid=${tokenData.openid}&lang=zh_CN`;
+      request(getInforUrl,function (error,response,body) {
+          if (!error && response.statusCode == 200) {
+                let userData = JSON.parse(body);
+                res.send(userData)
+            }
+    })
+  }],function(err,result){
+    if(err) throw err
+  })
+
+}
--- wx_server.js
+++ wx_server.js
@@ -2,6 +2,7 @@
 const express =  require('express'),
       bodyParser = require('body-parser'),
       wxSignature = require('./wx_signature'),
+      wxAuth = require('./wx_auth'),
       app = express();
 
 //微信2小时刷新(动态获取access_token以及jsapi_ticket)
@@ -14,11 +15,17 @@ const express =  require('express'),
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-//单个路由(上传当前完整路径)
+//获取当前用户(上传当前完整路径)
 app.post('/wxSignature',function(req,res){
     let currentUrl = req.body.currentUrl;
     wxSignature.wxSignature(req,res,currentUrl) //签名
 })
+
+//获取当前用户微信的信息
+app.post('/wxUserInfor',function(req,res){
+    wxAuth.getWxUserInfor(req,res)
+})
+
 
 app.listen(5086,function(){
 	console.log('微信签名服务器已经启动');
